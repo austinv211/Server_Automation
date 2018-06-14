e57e1650fdaee46aa1a4d4e246717133b0b8eb32
Austin Vargason <vargasona@gmail.com>
2018-06-13 17:59:06 -0700

    working dvd drive letter change

--- src/serverautomation.ps1
+++ src/serverautomation.ps1
@@ -62,12 +62,49 @@ function Set-NICBindingOrder {
     begin {
         #get current binding order
         Get-NetAdapterBinding
+
+        #TODO: finish figuring out netadapter binding order
     }
     process {
 
     }
 }
 
+#TODO: Finish NIC Card functions
+
+#function to change the drive letter of the dvd drive to Z
+function Set-DVDDriveLetter {
+    [CmdletBinding()]
+
+    param ( 
+        [Parameter(Mandatory=$true)]
+        [string]$DriveLetter
+    )
+
+    begin {
+        #get the current drive letter of the dvd drive
+        $cdDriveLetter = Get-WmiObject -Class win32_cdromdrive | Select-Object -ExpandProperty Drive
+        $cdDisk = Get-WmiObject -Class win32_volume | Where-Object {$_.DriveLetter -eq $cdDriveLetter}
+    }
+    process {
+        if ($cdDriveLetter -eq $DriveLetter) {
+            Write-Host "Drive letter already set to $DriveLetter" -ForegroundColor Green;
+        }
+        else {
+            $cdDisk | Set-WmiInstance -Arguments @{DriveLetter = $DriveLetter}
+        }
+    }
+}
+
+#invoke functions in correct order with correct parameters
+
+#NIC options
+#set NIC powermanagement to disabled
+Set-NICPowerSaving -setEnabled $false
+#insert call to change NIC binding order
+
+#change the DVD Drive letter to Z
+Set-DVDDriveLetter -DriveLetter "Z:"
 
 
 
